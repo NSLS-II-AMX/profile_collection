d18458e45a0d209cb6aaf46253f5485ae8616862
Bruno Martins <bmartins@bnl.gov>
2016-04-15 10:41:54 -0400

    Add simple_ascan macro

--- startup/99-macros.py
+++ startup/99-macros.py
@@ -1,2 +1,13 @@
-def my_macro():
-    print('Here is your macro, enjoy!')
+def simple_ascan(camera, stats, motor, start, end, steps):
+    gs.DETS = [camera]
+    gs.MASTER_DET = camera
+ 
+    stats_name = "_".join((camera.name,stats)) 
+    gs.PLOT_Y = stats_name
+
+    uid = ascan(motor, start, end, steps)[0]
+    table = get_table(db[uid])
+    try:
+        return table[[motor.name, stats_name]]
+    except KeyError:
+        return table[[motor.name+"_readback", stats_name]]
