c058a8990ae63a7047d89b4ef0a39b35d8a64122
Bruno Martins <bmartins@bnl.gov>
2016-07-06 18:37:14 -0400

    Change simple_ascan to accept empty stats name

--- startup/99-macros.py
+++ startup/99-macros.py
@@ -2,7 +2,7 @@ def simple_ascan(camera, stats, motor, start, end, steps):
     gs.DETS = [camera]
     gs.MASTER_DET = camera
  
-    stats_name = "_".join((camera.name,stats)) 
+    stats_name = "_".join((camera.name,stats)) if stats else camera.name
     gs.PLOT_Y = stats_name
 
     uid = ascan(motor, start, end, steps)[0]
