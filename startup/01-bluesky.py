8d9213411798786c2a73e1902317a27ac8f9dd9d
Bruno Martins <bmartins@bnl.gov>
2016-08-05 16:32:45 -0400

    Only run install_qt_kicker if DISPLAY is set

--- startup/01-bluesky.py
+++ startup/01-bluesky.py
@@ -4,11 +4,13 @@ from bluesky.plans import *
 from bluesky.spec_api import *
 from bluesky.callbacks import *
 from bluesky.callbacks.olog import logbook_cb_factory
-from bluesky.utils import install_qt_kicker
 
-# The following line allows bluesky and pyqt4 GUIs to play nicely together:
-install_qt_kicker()
-
+# Only install_qt_kicker if DISPLAY is set
+import os
+if "DISPLAY" in os.environ:
+    from bluesky.utils import install_qt_kicker
+    # The following line allows bluesky and pyqt4 GUIs to play nicely together:
+    install_qt_kicker()
 
 # Subscribe metadatastore to documents.
 # If this is removed, data is not saved to metadatastore.
