1ca21f5115de6bbc0642434def69b5e642e95f9b
Bruno Martins <bmartins@bnl.gov>
2016-11-07 13:08:44 -0500

    Add scan_bpm function

--- startup/99-macros.py
+++ startup/99-macros.py
@@ -11,3 +11,28 @@ def simple_ascan(camera, stats, motor, start, end, steps):
         return table[[motor.name, stats_name]]
     except KeyError:
         return table[[motor.name+"_readback", stats_name]]
+
+
+def scan_bpm(bpm_num, bpm_motor, start, end, step):
+    detector_name = 'bpm' + str(bpm_num)
+    device_name = 'mbpm' + str(bpm_num)
+    motor_name = '_'.join((device_name, bpm_motor))
+
+    detector = globals()[detector_name]
+    device = globals()[device_name]
+    motor = getattr(device, bpm_motor)
+    egu = motor.motor_egu.get()
+
+    plan = scan([detector], motor, start, end, step)
+    result_uid = RE(plan)
+    table = db.get_table(db[result_uid])
+    channels = ['A', 'B', 'C', 'D']
+    ydata = [table['_'.join((detector_name, ch.lower()))] for ch in channels]
+    xdata = table[motor_name]
+
+    plt.xlabel('Motor position ({})'.format(egu))
+    plt.ylabel('Currents')
+    plt.title('BPM {} Currents vs Motor Position'.format(bpm_num))
+    plots = [plt.plot(xdata, ydata[i], label=ch) for i, ch in enumerate(channels)]
+    plt.legend(channels)
+    return result_uid
