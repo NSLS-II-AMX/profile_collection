a89c6d10236727e1491766eeade93203655d605c
Bruno Martins <bmartins@bnl.gov>
2016-11-18 10:55:17 -0500

    Add more ROIs to DXP

--- startup/22-dxp.py
+++ startup/22-dxp.py
@@ -4,4 +4,5 @@ class AMXMercury(Mercury1, SoftDXPTrigger):
     pass
 
 mercury = AMXMercury('XF:17IDB-ES:AMX{Det:Mer}', name='mercury')
-mercury.read_attrs = ['mca.spectrum', 'mca.preset_live_time', 'mca.rois.roi0.count']
+mercury.read_attrs = ['mca.spectrum', 'mca.preset_live_time', 'mca.rois.roi0.count',
+                      'mca.rois.roi1.count', 'mca.rois.roi2.count', 'mca.rois.roi3.count']
