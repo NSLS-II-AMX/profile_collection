6d344b441968596603ab06128a4982ad4c6b8016
Maksim Rakitin <mrakitin@bnl.gov>
2019-07-10 17:08:19 -0400

    Fixes for running kernels in Jupyter (disable mpl in 00-base.py)

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,7 +1,5 @@
-print(f'Loading {__file__}')
-
 import nslsii
-nslsii.configure_base(get_ipython().user_ns, 'amx')
+nslsii.configure_base(get_ipython().user_ns, 'amx', mpl=False)
 # nslsii.configure_olog(get_ipython().user_ns, subscribe=False)
 
 #Optional: set any metadata that rarely changes.
