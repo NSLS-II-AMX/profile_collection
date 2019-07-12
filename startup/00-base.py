c556676d441b32319b67288c53444ad643924899
Maksim Rakitin <mrakitin@bnl.gov>
2019-07-12 06:49:20 -0400

    DAMA changes from 2019-07-10 for JL live plotting

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,8 +1,9 @@
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'notebook') # i.e. %matplotlib notebook
+
 import nslsii
-nslsii.configure_base(get_ipython().user_ns, 'amx', mpl=False)
+nslsii.configure_base(get_ipython().user_ns, 'amx', bec=True, mpl=True, pbar=False)
 # nslsii.configure_olog(get_ipython().user_ns, subscribe=False)
 
 #Optional: set any metadata that rarely changes.
 RE.md['beamline_id'] = 'AMX'
-
-
