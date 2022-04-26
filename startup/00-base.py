135a2209ec180490146878af5272be493837e583
Maksim Rakitin <mrakitin@bnl.gov>
2022-04-26 16:24:14 -0400

    Updates for 2022-2.0-py39 env/kernel to work

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,11 +1,27 @@
 print(f"Loading {__file__}")
 
+import os
+import uuid
+from pathlib import Path
+
+import appdirs
+import matplotlib
+import matplotlib.pyplot
+import nslsii
+import numpy as np
+from IPython import get_ipython
+from ophyd import Component, Device, EpicsSignal
+from ophyd.areadetector.filestore_mixins import resource_factory
+from ophyd.signal import EpicsSignalBase
+
 try:
     ###############################################################################
     # TODO: remove this block once https://github.com/bluesky/ophyd/pull/959 is
     # merged/released.
     from datetime import datetime
-    from ophyd.signal import EpicsSignalBase, EpicsSignal, DEFAULT_CONNECTION_TIMEOUT
+
+    from ophyd.signal import (DEFAULT_CONNECTION_TIMEOUT, EpicsSignal,
+                              EpicsSignalBase)
 
     def print_now():
         return datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S.%f')
@@ -38,80 +54,18 @@ try:
     ###############################################################################
 
     from ophyd.signal import EpicsSignalBase
+
     # EpicsSignalBase.set_default_timeout(timeout=10, connection_timeout=10)  # old style
     EpicsSignalBase.set_defaults(timeout=10, connection_timeout=10)  # new style
 
 except ImportError:
     pass
 
-import os
-import matplotlib
-from IPython import get_ipython
+nslsii.configure_base(get_ipython().user_ns, 'amx', bec=True, pbar=False,
+                      publish_documents_with_kafka=True)
 
-get_ipython().run_line_magic('matplotlib', 'widget')  # i.e. %matplotlib widget
-import matplotlib.pyplot
-
-from ophyd import Device, Component, EpicsSignal
-from ophyd.signal import EpicsSignalBase
-from ophyd.areadetector.filestore_mixins import resource_factory
-import uuid
-import os
-from pathlib import Path
-import numpy as np
-import appdirs
-
-# Set up a RunEngine and use metadata backed by a sqlite file.
-from bluesky import RunEngine
-RE = RunEngine()
-
-import nslsii
-nslsii.configure_base(get_ipython().user_ns, 'amx', bec=True, pbar=False, publish_documents_with_kafka=True)
-
-# Set up SupplementalData.
-from bluesky import SupplementalData
-sd = SupplementalData()
-RE.preprocessors.append(sd)
-
-# Set up a Broker.
-from databroker import Broker
-db = Broker.named('amx')
-
-# and subscribe it to the RunEngine
-RE.subscribe(db.insert)
-
-# Add a progress bar.
-# from bluesky.utils import ProgressBarManager
-# pbar_manager = ProgressBarManager()
-# RE.waiting_hook = pbar_manager
-
-# Register bluesky IPython magics.
-from bluesky.magics import BlueskyMagics
-get_ipython().register_magics(BlueskyMagics)
-
-# Set up the BestEffortCallback.
-from bluesky.callbacks.best_effort import BestEffortCallback
-bec = BestEffortCallback()
-bec.overplot = False
-RE.subscribe(bec)
-peaks = bec.peaks
-
-# Import matplotlib and put it in interactive mode.
-import matplotlib.pyplot as plt
-plt.ion()
-
-# Make plots update live while scans run.
-from bluesky.utils import install_nb_kicker
-install_nb_kicker()
-
-# convenience imports
-# some of the * imports are for 'back-compatibility' of a sort -- we have
-# taught BL staff to expect LiveTable and LivePlot etc. to be in their
-# namespace
-import numpy as np
-
-import bluesky.plans as bp
-import bluesky.plan_stubs as bps
-import bluesky.preprocessors as bpp
+# Disable plots via BestEffortCallback:
+bec.disable_plots()
 
 try:
     from bluesky.utils import PersistentDict
