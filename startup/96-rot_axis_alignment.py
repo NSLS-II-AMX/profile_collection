f3035933be786238561c2fe98f218315d06e0dec
Kreitler, Dale <dkreitler@bnl.gov>
2022-09-21 20:11:37 -0400

    ready rot axis for actual use

--- startup/96-rot_axis_alignment.py
+++ startup/96-rot_axis_alignment.py
@@ -1,5 +1,17 @@
+print(f"Loading {__file__}")
+
 from ophyd import PseudoPositioner, PseudoSingle, Staged
 from ophyd.pseudopos import pseudo_position_argument, real_position_argument
+from ophyd import (
+    SingleTrigger,
+    ProsilicaDetector,
+    ColorConvPlugin,
+    Device,
+)
+from ophyd import Component as Cpt
+from ophyd.areadetector.plugins import PluginBase, register_plugin
+from ophyd.areadetector.base import DDC_EpicsSignal, DDC_EpicsSignalRO
+from ophyd.areadetector.base import ADComponent as ADCpt
 
 
 class GonioCameraPositioner(PseudoPositioner):
@@ -41,10 +53,6 @@ class GonioCameraPositioner(PseudoPositioner):
     def update_parameters(self, *args, **kwargs):
         [_.sync() for _ in self._pseudo]
 
-    def update_origin(self):
-        self._y0.set(self.real_y.user_readback.get())
-        self._z0.set(self.real_z.user_readback.get())
-
     def unstage(self):
 
         self.log.debug("Unstaging %s", self.name)
@@ -60,7 +68,9 @@ class GonioCameraPositioner(PseudoPositioner):
 
         # Restore original values.
         for sig, val in reversed(list(self._original_vals.items())):
-            self.log.debug("Setting %s back to its original value: %r", sig.name, val)
+            self.log.debug(
+                "Setting %s back to its original value: %r", sig.name, val
+            )
             sig.set(val, settle_time=0.1).wait()
             self._original_vals.pop(sig)
         devices_unstaged.append(self)
@@ -74,8 +84,12 @@ class GonioCameraPositioner(PseudoPositioner):
         d = np.pi / 180
         omega = self.omega.get()
         return self.RealPosition(
-            real_y=(pos.cam_y * np.cos(omega * d) - pos.cam_z * np.sin(omega * d)),
-            real_z=(pos.cam_y * np.sin(omega * d) + pos.cam_z * np.cos(omega * d)),
+            real_y=(
+                pos.cam_y * np.cos(omega * d) - pos.cam_z * np.sin(omega * d)
+            ),
+            real_z=(
+                pos.cam_y * np.sin(omega * d) + pos.cam_z * np.cos(omega * d)
+            ),
         )
 
     @real_position_argument
@@ -84,10 +98,161 @@ class GonioCameraPositioner(PseudoPositioner):
         d = np.pi / 180
         omega = self.omega.get()
         return self.PseudoPosition(
-            cam_y=(pos.real_y) * np.cos(omega * d) + (pos.real_z) * np.sin(omega * d),
-            cam_z=-(pos.real_y) * np.sin(omega * d) + (pos.real_z) * np.cos(omega * d),
+            cam_y=(pos.real_y) * np.cos(omega * d)
+            + (pos.real_z) * np.sin(omega * d),
+            cam_z=-(pos.real_y) * np.sin(omega * d)
+            + (pos.real_z) * np.cos(omega * d),
         )
 
 
-gcp3 = GonioCameraPositioner("XF:17IDB-ES:AMX{Gon:1", name="gcp")
+@register_plugin
+class CVPlugin(PluginBase):
+    _default_suffix = "CV1:"
+    _suffix_re = "CV1\d:"
+    _default_read_attrs = ["outputs"]
+    func_sets = DDC_EpicsSignal(
+        *[(f"func_set{k}", f"CompVisionFunction{k}") for k in range(1, 4)]
+    )
+    inputs = DDC_EpicsSignal(
+        *[(f"input{k}", f"Input{k}") for k in range(1, 11)]
+    )
+    outputs = DDC_EpicsSignalRO(
+        *[(f"output{k}", f"Output{k}_RBV") for k in range(1, 11)]
+    )
+    cam_depth = ADCpt(EpicsSignal, "CompVisionCamDepth", kind="config")
 
+
+class SingleTriggerProsilica(SingleTrigger, ProsilicaDetector):
+    pass
+
+
+class RotAlignLowMag(StandardProsilica):
+    # cc1 = Cpt(ColorConvPlugin, "CC1:")
+    pix_per_um = Cpt(Signal, value=1, kind="config")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.read_attrs = ["stats1", "stats2"]
+        self.stats1.read_attrs = ["total"]
+        self.stats2.read_attrs = ["total"]
+        self.stage_sigs.cam = {
+            "acquire_time": 0.0025,
+            "acquire_period": 1,
+        }
+
+
+class RotAlignHighMag(StandardProsilica):
+    cc1 = Cpt(ColorConvPlugin, "CC1:")
+    cv1 = Cpt(CVPlugin, "CV1:")
+    cam_mode = Cpt(Signal, value=None, kind="config")
+    pix_per_um = Cpt(Signal, value=3.4, kind="config")
+    roi_offset = Cpt(Signal, value=256, kind="config")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.read_attrs = [
+            "cc1",
+            "cv1",
+            "roi1",
+            "roi2",
+            "roi3",
+            "roi4",
+            "stats4",
+            "trans1",
+        ]
+
+        [
+            _plugin.enable_on_stage()
+            for _plugin in [
+                self.cv1,
+                self.roi4,
+                self.stats1,
+                self.stats4,
+                self.proc1,
+                self.cc1,
+            ]
+        ]
+        self.cv1.outputs.read_attrs = [
+            "output7",
+            "output8",
+        ]  # 7 left, 8 right pixel
+        self.stats4.read_attrs = ["centroid", "max_xy"]
+        self.stats4.centroid.read_attrs = ["x", "y"]
+        self.stats4.max_xy.read_attrs = ["x", "y"]
+        self.cam_mode.subscribe(self._update_stage_sigs, event_type="value")
+        self._update_stage_sigs()
+        self.roi1.min_xyz.min_y.subscribe(self._sync_rois, event_type="value")
+
+    def _update_stage_sigs(self, *args, **kwargs):
+        self.stage_sigs.clear()
+        self.stage_sigs.update(
+            [
+                ("cam.acquire", 0),
+                ("cam.image_mode", 1),
+            ]
+        )
+        if self.cam_mode.get() == "edge_detection":
+            self.stage_sigs.update(
+                [
+                    ("cam.acquire_time", 0.15),
+                    ("cam.acquire_period", 0.15),
+                    ("cv1.nd_array_port", "ROI4"),
+                    ("cv1.func_sets.func_set1", "Canny Edge Detection"),
+                    ("cv1.inputs.input1", 35),
+                    ("cv1.inputs.input2", 5),
+                    ("cv1.inputs.input3", 13),
+                    ("cv1.inputs.input4", 5),
+                    ("roi4.min_xyz.min_x", 672),
+                    ("roi4.min_xyz.min_y", 0),
+                    ("roi4.size.x", 898),
+                    ("roi4.size.y", 1246),
+                ]
+            )
+
+        elif self.cam_mode.get() == "rot_align":
+            self.stage_sigs.update(
+                [
+                    ("cam.acquire_time", 0.15),
+                    ("cam.acquire_period", 0.15),
+                    ("proc1.nd_array_port", "CC1"),
+                    ("proc1.enable_filter", 1),
+                    ("proc1.filter_type", "CopyToFilter"),
+                    ("proc1.o_scale", -1),
+                    ("proc1.o_offset", 140),
+                    ("roi4.min_xyz.min_x", 1117),
+                    ("roi4.min_xyz.min_y", 0),
+                    ("roi4.size.x", 25),
+                    ("roi4.size.y", 1246),
+                ]
+            )
+
+    def _sync_rois(self, *args, **kwargs):
+        self.roi2.min_xyz.min_y.put(
+            self.roi1.min_xyz.min_y.get() - self.roi_offset.get()
+        )
+
+
+class RotationAxisAligner(Device):
+    cam_hi = Cpt(RotAlignHighMag, "{Cam:7}")
+    cam_lo = Cpt(RotAlignLowMag, "{Cam:6}")
+    gc_positioner = Cpt(GonioCameraPositioner, "{Gon:1")
+    current_rot_axis = Cpt(
+        Signal,
+        value=None,
+        doc="current rotation axis in pixels based on high mag ROI",
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.current_rot_axis.put(self.cam_hi.roi1.min_xyz.min_y.get() + 256)
+        self.current_rot_axis.subscribe(
+            self._update_roi, event_type="value", run=False
+        )
+
+    def _update_roi(self, *args, **kwargs):
+        self.cam_hi.roi1.min_xyz.min_y.put(
+            round(self.current_rot_axis.get()) - 256
+        )
+
+
+rot_aligner = RotationAxisAligner("XF:17IDB-ES:AMX", name="rot_aligner")
--- startup/97-plan_development.py
+++ startup/97-plan_development.py
@@ -1,86 +1,108 @@
 # algorithm development at the beamline
 
 
-def test_plan3():
-    
-    #find optimal omega for sheath opening
-    omega_scan_uid = yield from bp.scan([cam_6],gonio.o,-10,100,20)
+def measure_rot_axis(
+    detector, rot_motor, start, *, sweep_width=270, n_steps=4
+):
+    detector.cam_mode.put("rot_align")
+    scan_uid = yield from bp.scan(
+        [detector], rot_motor, start, start + sweep_width, n_steps
+    )
+    b = db[scan_uid].table()[f"{detector.stats4.max_xy.y.name}"]
+    A = np.matrix(
+        [
+            [np.cos(np.deg2rad(omega)), np.sin(np.deg2rad(omega)), 1]
+            for omega in np.linspace(*(start, start + sweep_width, n_steps))
+        ]
+    )
+    p = (
+        np.linalg.inv(A.transpose() * A)
+        * A.transpose()
+        * np.matrix(b.to_numpy()).transpose()
+    )
+    delta_y_pix, delta_z_pix, rot_axis_pix = p[0], p[1], p[2]
+    delta_y, delta_z = (
+        delta_y_pix / detector.pix_per_um.get(),
+        delta_z_pix / detector.pix_per_um.get(),
+    )
+    return delta_y, delta_z, rot_axis_pix
+
+
+def measure_tip_dist(detector, *, coarse_align_pix=215):
+    yield from bps.abs_set(detector.cam_mode, "edge_detection")
+    scan_uid = yield from bp.count([detector], 1)
+    left_pixel = db[scan_uid].table()[f"{detector.cv1.outputs.output7.name}"][
+        1
+    ]
+    delta_x_pix = left_pixel - coarse_align_pix
+    return delta_x_pix / detector.pix_per_um.get()
+
+
+def rot_pin_align(rot_aligner, rot_motor, long_motor):
+
+    # find optimal omega for sheath opening
+    omega_scan_uid = yield from bp.scan(
+        [rot_aligner.cam_lo], rot_motor, -10, 100, 20
+    )
     omega_scan_df = db[omega_scan_uid].table()
-    omega_start = omega_scan_df['gonio_o'][omega_scan_df['cam_6_stats1_total'].idxmax()]
-    yield from bps.mv(gonio.o, omega_start)
-    omega_list = [omega_start + _ for _ in [0, 90, 180, 270]]
-    
-    #1st approximate rotation
-    scan_uid = yield from bp.list_scan([cam_7], gonio.o, omega_list)
-    d = np.pi/180
-    A = np.matrix([[np.cos(omega*d), np.sin(omega*d), 1] for omega in omega_list])
-    b = db[scan_uid].table()['cam_7_stats4_max_xy_y']
-    print(f'b: {b}')
-    p0 = np.linalg.inv(A.transpose()*A)*A.transpose()*np.matrix(b.to_numpy()).transpose()
-    delta_y_pix = p0[0]
-    delta_z_pix = p0[1]
-    rot_y_pix = p0[2]
-    print(rot_y_pix)
-    pix_per_um = 3.4
-    delta_y_um = delta_y_pix / pix_per_um
-    delta_z_um = delta_z_pix / pix_per_um
-    yield from bps.mvr(gonio.py, -delta_y_um)
+    omega_start = omega_scan_df[f"{rot_motor.name}"][
+        omega_scan_df[f"{rot_aligner.cam_lo.stats1.total.name}"].idxmax()
+    ]
+    yield from bps.mv(rot_motor, omega_start)
+
+    # move pin tip past ROI
+    delta_long = yield from measure_tip_dist(rot_aligner.cam_hi)
+    yield from bps.mvr(long_motor, delta_long)
+
+    # first coarse alignment
+    delta_y, delta_z, rot_axis_pix = yield from measure_rot_axis(
+        rot_aligner.cam_hi, rot_motor, omega_start
+    )
+
+    # move to approximate rotation axis
+    yield from bps.mvr(rot_aligner.gc_positioner.real_y, -delta_y)
     yield from bps.sleep(0.1)
-    yield from bps.mvr(gonio.pz, -delta_z_um)
-    
-    scan_uid = yield from bp.list_scan([cam_7], gonio.o, omega_list)
-    d = np.pi/180
-    A = np.matrix([[np.cos(omega*d), np.sin(omega*d), 1] for omega in omega_list])
-    b = db[scan_uid].table()['cam_7_stats4_max_xy_y']
-    print(f'b: {b}')
-    p0 = np.linalg.inv(A.transpose()*A)*A.transpose()*np.matrix(b.to_numpy()).transpose()
-    delta_y_pix = p0[0]
-    delta_z_pix = p0[1]
-    rot_y_pix = p0[2]
-    print(rot_y_pix)
-    pix_per_um = 3.4
-    delta_y_um = delta_y_pix / pix_per_um
-    delta_z_um = delta_z_pix / pix_per_um
-    yield from bps.mvr(gonio.py, -delta_y_um)
+    yield from bps.mvr(rot_aligner.gc_positioner.real_z, -delta_z)
+
+    # move closer to pin tip for new measurement
+    yield from bps.mvr(gonio.gx, -55)
+
+    # second alignment
+    delta_y, delta_z, rot_axis_pix = yield from measure_rot_axis(
+        rot_aligner.cam_hi, rot_motor, omega_start
+    )
+    yield from bps.mvr(rot_aligner.gc_positioner.real_y, -delta_y)
     yield from bps.sleep(0.1)
-    yield from bps.mvr(gonio.pz, -delta_z_um)
-    
-    yield from bps.mvr(gonio.gx,-25)
-    
-    scan_uid = yield from bp.list_scan([cam_7], gonio.o, omega_list)
-    d = np.pi/180
-    A = np.matrix([[np.cos(omega*d), np.sin(omega*d), 1] for omega in omega_list])
-    b = db[scan_uid].table()['cam_7_stats4_max_xy_y']
-    print(f'b: {b}')
-    p0 = np.linalg.inv(A.transpose()*A)*A.transpose()*np.matrix(b.to_numpy()).transpose()
-    delta_y_pix = p0[0]
-    delta_z_pix = p0[1]
-    rot_y_pix = p0[2]
-    print(rot_y_pix)
-    pix_per_um = 3.4
-    delta_y_um = delta_y_pix / pix_per_um
-    delta_z_um = delta_z_pix / pix_per_um
-    yield from bps.mvr(gonio.py, -delta_y_um)
-    yield from bps.sleep(0.1)
-    yield from bps.mvr(gonio.pz, -delta_z_um)
-    
-    yield from bps.mv(gonio.o, omega_start)
-    scan_uid = yield from bp.rel_scan([cam_7,gcp3.real_y,gcp3.real_z], gcp3.cam_y, -5, 5, 20)
-    scan_df = db[scan_uid].table()
-    b = scan_df['cam_7_stats4_max_xy_y']
-    A = np.matrix([[cam_y, 1] for cam_y in scan_df['gcp_cam_y']])
-    v0 = np.linalg.inv(A.transpose()*A)*A.transpose()*np.matrix(b.to_numpy()).transpose()
-    best_cam_y = (p0[2] - v0[1]) / v0[0]
-    print(best_cam_y)
-    yield from bps.mv(gcp3.cam_y,best_cam_y)
-    
-    yield from bps.mv(gonio.o, omega_start + 90)
-    scan_uid = yield from bp.rel_scan([cam_7,gcp3.real_y,gcp3.real_z], gcp3.cam_y, -5, 5, 20)
-    scan_df = db[scan_uid].table()
-    b = scan_df['cam_7_stats4_max_xy_y']
-    A = np.matrix([[cam_y, 1] for cam_y in scan_df['gcp_cam_y']])
-    v0 = np.linalg.inv(A.transpose()*A)*A.transpose()*np.matrix(b.to_numpy()).transpose()
-    best_cam_y = (p0[2] - v0[1]) / v0[0]
-    
-    print(best_cam_y)
-    yield from bps.mv(gcp3.cam_y,best_cam_y)
+    yield from bps.mvr(rot_aligner.gc_positioner.real_z, -delta_z)
+
+    # third alignment, do not attempt to move, just measure
+    _, _, rot_axis_pix = yield from measure_rot_axis(
+        rot_aligner.cam_hi, rot_motor, omega_start
+    )
+
+    def tune_pin_inner(cam_axis):
+        yield from bps.abs_set(rot_aligner.cam_hi.cam_mode, "rot_align")
+        scan_uid = yield from bp.rel_scan(
+            [rot_aligner.cam_hi], cam_axis, -5, 5, 20
+        )
+        scan_df = db[scan_uid].table()
+        b = scan_df[f"{rot_aligner.cam_hi.stats4.max_xy.y.name}"]
+        A = np.matrix([[pos, 1] for pos in scan_df[f"{cam_axis.name}"]])
+        p = (
+            np.linalg.inv(A.transpose() * A)
+            * A.transpose()
+            * np.matrix(b.to_numpy()).transpose()
+        )
+        best_cam_pos = (rot_axis_pix - p[1]) / p[0]
+        return best_cam_pos
+
+    best_cam_y = yield from tune_pin_inner(rot_aligner.gc_positioner.cam_y)
+    yield from bps.mv(rot_aligner.gc_positioner.cam_y, best_cam_y)
+    yield from bps.mvr(gonio.o, 90)
+    best_cam_y = yield from tune_pin_inner(rot_aligner.gc_positioner.cam_y)
+    yield from bps.mv(rot_aligner.gc_positioner.cam_y, best_cam_y)
+
+    if abs(rot_axis_pix - rot_aligner.current_rot_axis.get()) < 20:
+        yield from bps.abs_set(
+            rot_aligner.current_rot_axis, rot_axis_pix.item(0)
+        )
