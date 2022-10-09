5eb8142d0ecaf028617a2a6070bae8f5bb3b9f78
Kreitler, Dale <dkreitler@bnl.gov>
2022-10-09 11:04:59 -0400

    Fix errors in KBTweaker device *fix voltage limit bounds *fix deadband function

--- startup/96-beam_alignment.py
+++ startup/96-beam_alignment.py
@@ -39,33 +39,18 @@ class SpecialProsilica(ProsilicaDetector):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.read_attrs = ["stats4", "trans1", "cc1", "proc1", "roi1", "roi4"]
         self.cc1.enable_on_stage()
         self.proc1.enable_on_stage()
-        self.stats4.read_attrs = ["total", "net"]
+        self.read_attrs = ["stats4"]
+        self.stats4.read_attrs = ["total", "net", "centroid"]
         self.stats4.centroid.read_attrs = ["x", "y"]
-
-    def stage(self):
-        # settings for smoother centroid fitting
-        self.proc1.enable_filter.put("1")
-        self.proc1.filter_type.put("RecursiveAve")
-        self.proc1.num_filter.put("5")
-        # color conversion to monochromatic for stats plugin
-        self.cc1.nd_array_port.put("PROC1")
-        self.trans1.nd_array_port.put("CC1")
-        # ROI4 must match ROI1, which defines center of rotation
-        self.roi4.min_xyz.min_x.put(f"{self.roi1.min_xyz.min_x.get()}")
-        self.roi4.min_xyz.min_y.put(f"{self.roi1.min_xyz.min_y.get()}")
-        super().stage()
-
-    def unstage(self):
-        # process plugin
-        self.proc1.num_filter.put("1")
-        self.proc1.enable_filter.put("0")
-        # put ports back for LSDC
-        self.trans1.nd_array_port.put("CAM")
-        self.cc1.nd_array_port.put("CAM")
-        super().unstage()
+        self.stage_sigs.update(
+            [
+                ("cc1.nd_array_port", "CC1"),
+                ("roi4.min_xyz.min_x", 818),
+                ("roi4.min_xyz.min_y", 400),
+            ]
+        )
 
 
 class KBTweakerAxis(Device):
@@ -75,11 +60,6 @@ class KBTweakerAxis(Device):
 
     def __init__(self, *args, voltage_limit=2, **kwargs):
         super().__init__(*args, **kwargs)
-        # do not mutate the hardware in device init, it makes it
-        # makes profile start up have side-effects which might mess up
-        # running experiments
-        #  self.step_size.put('0.05')
-
         self.voltage_limit = voltage_limit
         self._status = None
 
@@ -87,12 +67,13 @@ class KBTweakerAxis(Device):
         if self._status is not None and not self._status.done:
             raise RuntimeError("Another set is in progress")
 
-        if -self.voltage_limit < target < self.voltage_limit:
+        if self.voltage_limit < target or target < -self.voltage_limit:
             raise ValueError(
                 f"You passed {target!r} with out of ±{self.voltage_limit} gamut."
             )
+
         # get the step size
-        step_size = self.step_size.get()
+        step_size = float(self.step_size.get())
         # get the current voltage
         current = self.voltage.get()
         # compute (float) number of "steps" to take
@@ -104,18 +85,10 @@ class KBTweakerAxis(Device):
                 return True
             return False
 
-        self._status = status = SubscriptionStatus(self, deadband)
+        self._status = status = SubscriptionStatus(self.voltage, deadband)
         if not status.done:
             self.actuate.put(num_steps)
         return status
-
-    def stop(self, *, success=False):
-        # TODO do something to actually stop the voltage moving it that is
-        # possible?
-        super().stop(success)
-        if self._status is not None and not self._status.done:
-            self._status.set_exception(Exception("Motion stopped"))
-            self._status = None
 
 
 class KBTweaker(Device):
@@ -125,4 +98,4 @@ class KBTweaker(Device):
 
 kbt = KBTweaker("XF:17IDB-BI:AMX{Best:2}", name="kbt")
 
-cam_hi = SpecialProsilica("XF:17IDB-ES:AMX{Cam:7}", name="cam_hi")
+cam_hi_ba = SpecialProsilica("XF:17IDB-ES:AMX{Cam:7}", name="cam_hi")
