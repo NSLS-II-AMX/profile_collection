4f98adc338165c6fa20af0e99898a985ae662a06
Kreitler, Dale <dkreitler@bnl.gov>
2023-03-07 21:12:15 -0500

    Redo piezo tweaker with new EPICS PVs *steps towards attenuator positioner

--- startup/96-beam_alignment.py
+++ startup/96-beam_alignment.py
@@ -28,7 +28,10 @@ from ophyd.areadetector.filestore_mixins import (
 from ophyd import Component as Cpt
 from ophyd.status import SubscriptionStatus
 
-import time
+from ophyd.pv_positioner import (
+    PVPositionerComparator,
+    PVPositionerIsClose,
+)
 
 
 class SpecialProsilica(ProsilicaDetector):
@@ -55,7 +58,12 @@ class SpecialProsilica(ProsilicaDetector):
         )
 
 
-class KBTweakerAxis(Device):
+class KBTweakerAxis(PVPositionerIsClose):
+    setpoint = Cpt(EpicsSignal, "")
+    readback = Cpt(EpicsSignalRO, "RBV")
+    rtol = 0.01
+
+    """
     voltage = Cpt(EpicsSignal, "", kind="hinted")
     step_size = Cpt(EpicsSignal, ".INPA", kind="config")
     actuate = Cpt(EpicsSignal, ".B", kind="omitted")
@@ -98,13 +106,80 @@ class KBTweakerAxis(Device):
                 self.actuate.put(step_direction)
                 time.sleep(1)
         return status
+        """
 
 
 class KBTweaker(Device):
-    hor = Cpt(KBTweakerAxis, ":TwkCh1")
-    ver = Cpt(KBTweakerAxis, ":TwkCh2")
+    hor = Cpt(KBTweakerAxis, "-Motor-X}Mtr")
+    ver = Cpt(KBTweakerAxis, "-Motor-Y}Mtr")
 
 
-kbt = KBTweaker("XF:17IDB-BI:AMX{Best:2}", name="kbt")
+class MXAttenuator(PVPositionerComparator):
+    setpoint = Cpt(EpicsSignal, "Trans-SP")
+    readback = Cpt(EpicsSignalRO, "Trans-I")
+    actuate = Cpt(EpicsSignal, "Cmd:Set-Cmd.PROC")
+    atten1 = Cpt(
+        EpicsMotor,
+        "XF:17IDB-OP:AMX{Attn:BCU-Ax:1}Mtr",
+        add_prefix="",
+        doc="150 um Sn",
+    )
+    atten2 = Cpt(
+        EpicsMotor,
+        "XF:17IDB-OP:AMX{Attn:BCU-Ax:2}Mtr",
+        add_prefix="",
+        doc="50 um Al",
+    )
+    atten3 = Cpt(
+        EpicsMotor,
+        "XF:17IDB-OP:AMX{Attn:BCU-Ax:3}Mtr",
+        add_prefix="",
+        doc="6 um Al",
+    )
+    atten4 = Cpt(
+        EpicsMotor,
+        "XF:17IDB-OP:AMX{Attn:BCU-Ax:4}Mtr",
+        add_prefix="",
+        doc="20 um Sn",
+    )
+    rtol = 0.01
+    atol = None
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def done_comparator(self, readback: float, setpoint: float) -> bool:
+        """
+        Check if the readback is close to the setpoint value.
+        Uses numpy.isclose to make the comparison. Tolerance values
+        atol and rtol for numpy.isclose are taken from the attributes
+        self.atol and self.rtol, which can be defined as class attributes
+        or passed in as init parameters.
+        If atol or rtol are omitted, the default values from numpy are
+        used instead.
+        """
+        kwargs = {}
+        if self.atol is not None:
+            kwargs["atol"] = self.atol
+        if self.rtol is not None:
+            kwargs["rtol"] = self.rtol
+
+        sigs = [self.atten1, self.atten2, self.atten3, self.atten4]
+
+        return all(
+            [
+                np.isclose(
+                    np.float64(sig.user_setpoint.get()),
+                    np.float64(sig.user_readback.get()),
+                    **kwargs
+                )
+                for sig in sigs
+            ]
+        )
+
+
+mxatten = MXAttenuator(
+    "XF:17IDB-OP:AMX{Attn:BCU}", name="mxatten", settle_time=5
+)
+kbt = KBTweaker("XF:17ID-ES:AMX{Best:2", name="kbt")
 cam_hi_ba = SpecialProsilica("XF:17IDB-ES:AMX{Cam:7}", name="cam_hi")
