9b2de114daa638e65a50faf5a0e560a689e549fb
Kreitler, Dale <dkreitler@bnl.gov>
2023-06-20 14:14:43 -0400

    Add new devices for scanning off axis alignment camera *Zebra with additional signals for OR gates (for additional TTL3 output)

--- startup/96-top_alignment.py
+++ startup/96-top_alignment.py
@@ -22,6 +22,11 @@ class TopAlignCam(StandardProsilica):
     )
     cam_mode = Cpt(Signal, value=None, kind="config")
     pix_per_um = Cpt(Signal, value=0.164, doc="pixels per um")
+    out10_buffer = Cpt(EpicsSignalRO, "Out10:compress",
+                       doc="circular buffer for ADCompVision output10 (thresholding loop profile)")
+    out10_reset = Cpt(EpicsSignal, "Out10:compress.RES")
+    out9_buffer = Cpt(EpicsSignalRO, "Out9:compress")
+    out9_reset = Cpt(EpicsSignal, "Out9:compress.RES")
 
     def __init__(self, *args, **kwargs):
         super().__init__(
@@ -74,7 +79,80 @@ class TopAlignCam(StandardProsilica):
         super().stage(*args, **kwargs)
 
 
-topcam = TopAlignCam(
-    "XF:17IDB-ES:AMX{Cam:9}",
-    name="topcam",
-)
+class ZebraMXOr(Zebra):
+    or3 = Cpt(EpicsSignal, "OR3_ENA:B3")
+    or3loc = Cpt(EpicsSignal, "OR3_INP4")
+    armsel = Cpt(EpicsSignal, "PC_ARM_SEL")
+
+
+class RotationalAligner(Device):
+    zebra = Cpt(ZebraMXOr, 'XF:17IDB-ES:AMX{Zeb:2}:')
+    camera = Cpt(TopAlignCam, 'XF:17IDB-ES:AMX{Cam:9}', kind="hinted")
+    gonio_o = Cpt(EpicsMotor, "XF:17IDB-ES:AMX{Gon:1-Ax:O}Mtr")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.stage_sigs.update(
+            [
+                ("zebra.pos_capt.source", "Enc4"),
+                ("zebra.pos_capt.direction", 1),
+                ("zebra.armsel", 0),
+                ("zebra.pos_capt.gate.start", 179.9),
+                ("zebra.pos_capt.gate.width", 4.5),
+                ("zebra.pos_capt.gate.step", 9),
+                ("zebra.pos_capt.gate.num_gates", 20),
+                ("zebra.pos_capt.pulse.start", 0),
+                ("zebra.pos_capt.pulse.width", 4),
+                ("zebra.pos_capt.pulse.step", 5),
+                ("zebra.pos_capt.pulse.delay", 0),
+                ("zebra.pos_capt.pulse.max_pulses", 1),
+                ("zebra.or3", 1),
+                ("zebra.or3loc", 30),
+            ]
+        )
+        self.camera.stage_sigs.update(
+            [
+                ("cam.trigger_mode", 1),
+                ("cam.image_mode", 2),
+                ("cam.acquire", 1),
+            ]
+        )
+        self.read_attrs = ["camera.out10_buffer", "camera.out9_buffer"]
+
+    def stage(self, *args, **kwargs):
+        # self.camera.out10_reset.set(1)
+        # self.camera.out9_reset.set(1)
+        super().stage(*args, **kwargs)
+
+        def callback_armed(value, old_value, **kwargs):
+            if old_value == 0 and value == 1:
+                return True
+            else:
+                return False
+        callback_armed_status = SubscriptionStatus(
+            self.zebra.pos_capt.arm.output, callback_armed, run=False)
+        self.zebra.pos_capt.arm.arm.set(1, settle_time=0.5)
+        callback_armed_status.wait()
+        return callback_armed_status
+
+    def unstage(self, **kwargs):
+        super().unstage(**kwargs)
+        self.zebra.pos_capt.arm.disarm.set(1)
+
+    def trigger(self):
+        def callback_unarmed(value, old_value, **kwargs):
+            if old_value == 1 and value == 0:
+                return True
+            else:
+                return False
+        callback_unarmed_status = SubscriptionStatus(
+            self.zebra.pos_capt.arm.output, callback_unarmed, run=False)
+        self.gonio_o.set(0)
+        return(callback_unarmed_status)
+
+    # def read(self):
+    #
+    #    return(
+
+
+rot_align_test = RotationalAligner(name='rat')
