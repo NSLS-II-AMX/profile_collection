85ece0dee15ae5db653d7ae273aba92acf592286
Kreitler, Dale <dkreitler@bnl.gov>
2023-12-18 18:39:19 -0500

    Loop detection model-based centering -initialize devices and plan for loop centering -use mars gpu to run model on microscope jpg images

---
+++ startup/96-two_click_center.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 18 13:16:27 2023
+
+@author: dkreitler
+"""
+from collections import OrderedDict
+from ophyd.areadetector.filestore_mixins import FileStorePluginBase
+from ophyd.areadetector.plugins import JPEGPlugin
+from ophyd import Kind, DeviceStatus
+
+import requests
+
+
+class FileStoreJPEG(FileStorePluginBase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filestore_spec = "AD_JPEG"  # spec name stored in resource doc
+        self.stage_sigs.update(
+            [
+                ("file_template", "%s%s_%6.6d.jpg"),
+                ("file_write_mode", "Single"),
+            ]
+        )
+        # 'Single' file_write_mode means one image : one file.
+        # It does NOT mean that 'num_images' is ignored.
+
+    def get_frames_per_point(self):
+        return self.parent.cam.num_images.get()
+
+    def stage(self):
+        super().stage()
+        # this over-rides the behavior is the base stage
+        self._fn = self._fp
+
+        resource_kwargs = {
+            "template": self.file_template.get(),
+            "filename": self.file_name.get(),
+            "frame_per_point": self.get_frames_per_point(),
+        }
+        self._generate_resource(resource_kwargs)
+
+
+class JPEGPluginWithFileStore(JPEGPlugin, FileStoreJPEG):
+    pass
+
+
+class LoopDetector(Device):
+    url = Cpt(
+        Signal, value='http://mars8.nsls2.bnl.gov:8000/predict', kind='config'
+    )
+    filename = Cpt(Signal, value=None)
+    box = Cpt(Signal, value=[])
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.read_attrs = ['box']
+
+    def trigger(self):
+
+        filename_dict = {"file": Path(self.filename.get()).open('rb')}
+        response = requests.post(self.url.get(), files=filename_dict)
+        response.raise_for_status()
+        json_response = response.json()
+        if json_response['pred_boxes']:
+            self.box.put(response.json()['pred_boxes'][0]['box'])
+        else:
+            self.box.put([])
+        response_status = DeviceStatus(self.box, timeout=10)
+        response_status.set_finished()
+
+        return response_status
+
+
+class TwoClickLowMag(StandardProsilica):
+    cv1 = Cpt(CVPlugin, "CV1:")
+    cam_mode = Cpt(Signal, value=None, kind="config")
+    pix_per_um = Cpt(Signal, value=1, kind="config")
+
+    jpeg = Cpt(
+        JPEGPluginWithFileStore,
+        "JPEG1:",
+        write_path_template="/nsls2/data/amx/legacy/topcam",
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.read_attrs = ["cv1", "jpeg"]
+        self.jpeg.read_attrs = ['full_file_name']
+        self.cv1.read_attrs = ["outputs"]
+        self.cv1.outputs.read_attrs = ["output1", "output2", "output3"]
+        self.cam_mode.subscribe(self._update_stage_sigs, event_type="value")
+
+    def _update_stage_sigs(self, *args, **kwargs):
+        self.stage_sigs.clear()
+        self.stage_sigs.update(
+            [
+                ("cam.acquire", 0),
+                ("cam.image_mode", 1),
+                ("cam.acquire_time", 0.0025),
+                ("cam.acquire_period", 1),
+            ]
+        )
+        if self.cam_mode.get() == "centroid":
+            self.stage_sigs.update(
+                [
+                    ("cv1.enable", 1),
+                    ("cv1.func_sets.func_set2", "Centroid Identification"),
+                    ("cv1.inputs.input1", 1),
+                    ("cv1.inputs.input2", 5),
+                    ("cv1.inputs.input3", 30),
+                    ("cv1.inputs.input4", 3000000),
+                    ("cv1.inputs.input5", 5000),
+                ]
+            )
+        elif self.cam_mode.get() == "edge_detection":
+            self.stage_sigs.update(
+                [
+                    ("cv1.enable", 1),
+                    ("cv1.nd_array_port", "ROI4"),
+                    ("cv1.func_sets.func_set1", "Canny Edge Detection"),
+                    ("cv1.inputs.input1", 20),
+                    ("cv1.inputs.input2", 8),
+                    ("cv1.inputs.input3", 9),
+                    ("cv1.inputs.input4", 5),
+                    ("roi4.min_xyz.min_y", self.roi1.min_xyz.min_y.get()),
+                    (
+                        "roi4.min_xyz.min_x",
+                        self.roi1.min_xyz.min_x.get() + 245,
+                    ),
+                    ("roi4.size.x", 240),
+                    ("roi4.size.y", self.roi1.size.y.get()),
+                ]
+            )
+        elif self.cam_mode.get() == "two_click":
+            self.stage_sigs.update(
+                [
+                    ("jpeg.nd_array_port", "ROI2")
+                ]
+            )
+
+    def stage(self, *args, **kwargs):
+        self._update_stage_sigs(*args, **kwargs)
+        super().stage(*args, **kwargs)
+
+
+two_click_low = TwoClickLowMag("XF:17IDB-ES:AMX{Cam:6}", name="two_click_low")
+loop_detector = LoopDetector(name="loop_detector")
---
+++ startup/99-two_click_center_plan.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 18 15:54:11 2023
+
+@author: dkreitler
+"""
+
+
+def loop_detection_plan():
+
+    yield from bp.count([two_click_low], 1)
+    yield from bps.abs_set(
+        loop_detector.filename, two_click_low.jpeg.full_file_name.get()
+    )
+
+    scan_uid = yield from bp.count([loop_detector], 1)
+    box_coords = db[scan_uid].table()['loop_detector_box'][1]
+    mean_x = (box_coords[0] + box_coords[2]) / 2
+    mean_y = (box_coords[1] + box_coords[3]) / 2
+
+    delta_x = (mean_x - 320) * 2*two_click_low.pix_per_um.get()
+
+    delta_cam_y = (mean_y - 256) * 2*two_click_low.pix_per_um.get()
+
+    omega = gonio.o.user_readback.get()
+
+    d = np.pi/180
+
+    real_y = delta_cam_y * np.cos(omega * d)
+    real_z = delta_cam_y * np.sin(omega * d)
+
+    yield from bps.mvr(gonio.gx, delta_x)
+    yield from bps.mvr(gonio.py, -real_y)
+    yield from bps.mvr(gonio.pz, -real_z)
+
+    yield from bps.mvr(gonio.o, 90)
+
+    yield from bp.count([two_click_low], 1)
+    yield from bps.abs_set(
+        loop_detector.filename, two_click_low.jpeg.full_file_name.get()
+    )
+
+    scan_uid = yield from bp.count([loop_detector], 1)
+    box_coords = db[scan_uid].table()['loop_detector_box'][1]
+
+    mean_y = (box_coords[1] + box_coords[3]) / 2
+
+    delta_x = (mean_x - 320) * 2*two_click_low.pix_per_um.get()
+
+    delta_cam_y = (mean_y - 256) * 2*two_click_low.pix_per_um.get()
+
+    omega = gonio.o.user_readback.get()
+
+    d = np.pi/180
+
+    real_y = delta_cam_y * np.cos(omega * d)
+    real_z = delta_cam_y * np.sin(omega * d)
+
+    yield from bps.mvr(gonio.py, -real_y)
+    yield from bps.mvr(gonio.pz, -real_z)
+
+    yield from bps.mvr(gonio.o, -90)
