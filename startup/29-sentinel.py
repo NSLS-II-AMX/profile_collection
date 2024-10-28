85cd94cbe5de042ee2761294761907a37893993e
Kreitler, Dale <dkreitler@bnl.gov>
2024-10-28 16:34:15 -0400

    reliable home pins plan, tested with robot mounting

--- startup/29-sentinel.py
+++ startup/29-sentinel.py
@@ -6,5 +6,39 @@ Created on Thu Oct 17 15:43:19 2024
 @author: xf17id1
 """
 
+
+class PYZHomer(Device):
+
+    status = Cpt(EpicsSignalRO, "XF:17IDB-ES:AMX{Sentinel}Homing_Sts")
+    home_actuate = Cpt(EpicsSignal, "XF:17ID:AMX{Sentinel}pin_home")
+
+    kill_home = Cpt(EpicsSignal, "XF:17IDB-ES:AMX{Sentinel}Homing_Kill")
+    kill_py = Cpt(EpicsSignal, "XF:17IDB-ES:AMX{Gon:1-Ax:PY}Cmd:Kill-Cmd")
+    kill_pz = Cpt(EpicsSignal, "XF:17IDB-ES:AMX{Gon:1-Ax:PY}Cmd:Kill-Cmd")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def trigger(self):
+
+        def callback_homed(value, old_value, **kwargs):
+            if old_value == 1 and value == 0:
+                return True
+            else:
+                return False
+
+        self.home_actuate.put(1)
+
+        homing_status = SubscriptionStatus(
+            self.status,
+            callback_homed,
+            run=False,
+            timeout=180,
+        )
+
+        return homing_status
+
+
 goniomon = EpicsSignal("XF:17ID:AMX{Karen}goniomon", name="goniomon")
 govmon = EpicsSignal("XF:17ID:AMX{Karen}govmon", name="govmon")
+pyz_homer = PYZHomer("", name="pyz_homer")
---
+++ startup/99-plan_beamline_setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 15:59:31 2024
+
+@author: xf17id1
+"""
+
+from ophyd.utils.errors import StatusTimeoutError
+
+
+@reset_positions_decorator([govmon, goniomon])
+def home_pins():
+
+    yield from bps.mv(
+        govmon, 0,
+        goniomon, 0,
+        settle_time=1,
+    )
+
+    yield from bps.mv(
+        pyz_homer.kill_py, 1,
+        pyz_homer.kill_pz, 1,
+        settle_time=1,
+    )
+
+    yield from bps.mv(gonio.o, 90)
+
+    try:
+        yield from bp.count([pyz_homer], 1)
+
+    except StatusTimeoutError as e:
+        print(f'Caught {e} during pinYZ home attempt, retrying')
+
+        # kill 2x
+        yield from bps.mv(
+            pyz_homer.kill_py, 1,
+            pyz_homer.kill_pz, 1,
+            settle_time=1,
+        )
+        yield from bps.mv(
+            pyz_homer.kill_py, 1,
+            pyz_homer.kill_pz, 1,
+            settle_time=1,
+        )
+        yield from bp.count([pyz_homer, gonio.o], 1)
+
+    yield from bps.mv(gonio.o, 0)
+
+
+def test_home_pins(n):
+    for k in range(n):
+        yield from bps.abs_set(gov_rbt, 'SE', wait=True)
+        print(f'running test {k+1} of {n}')
+        robrob.mount_special(6, 1000000)
+        yield from home_pins()
+        yield from bps.abs_set(gov_rbt, 'SE', wait=True)
+        robrob.unmount_special(6, 1000000)
